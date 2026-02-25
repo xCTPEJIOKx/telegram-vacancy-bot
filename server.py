b05ec6bf48f5fe3109f22c34441b7fd01d45bbcd
xCTPEJIOKx <xctpejokx@users.noreply.github.com>
2026-02-25 09:15:45 +0700

    Fix default port

--- server.py
+++ server.py
@@ -383,5 +383,5 @@ def submit():
     return jsonify({'success': True})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
